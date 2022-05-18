04a91edc9a61b5d56875c48d80a5ebcf708663e2
Aлександр <sany_b@modimio.ru>
2022-05-18 23:30:58 +0300

    One Block

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#17181F',
+        white: '#EEEDF0',
+        primary: '#6C72CB',
+        secondary: '#CB69C1'
+      }
+    },
   },
   plugins: [],
 }
